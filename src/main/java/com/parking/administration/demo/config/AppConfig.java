f86f392f8bfec1f8594319baee3252e5251b82ff
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-04-09 21:09:06 -0300

    refactor: changing the location of the settings
    Creating new configs for the roles and changing some  location configs

--- src/main/java/com/parking/administration/demo/config/AppConfig.java
+++ src/main/java/com/parking/administration/demo/config/AppConfig.java
@@ -1,2 +1,57 @@
-package com.parking.administration.demo.config;public class AppConfig {
+package com.parking.administration.demo.config;
+
+import com.parking.administration.demo.infra.exception.UserNotFoundException;
+import com.parking.administration.demo.infra.exception.enums.ErrorCode;
+import com.parking.administration.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    private UserRepository repository;
+
+    public AppConfig(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public AppConfig() {
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return repository.findByEmail(username).orElseThrow(() -> new UserNotFoundException(
+                        ErrorCode.WA0003.getMessage(), ErrorCode.WA0003.getCode())
+                );
+            }
+        };
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+//    Using the configuration by SpringSecurity default, then it helps us to authenticate the user
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
--- src/main/java/com/parking/administration/demo/config/WebSecurityConfig.java
+++ src/main/java/com/parking/administration/demo/config/WebSecurityConfig.java
@@ -1,53 +1,89 @@
 package com.parking.administration.demo.config;
 
 import com.parking.administration.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static com.parking.administration.demo.domain.enums.Permission.*;
+import static com.parking.administration.demo.domain.enums.UserRole.ADMIN;
+import static com.parking.administration.demo.domain.enums.UserRole.USER;
+import static com.parking.administration.demo.utils.Constants.*;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"};
+
     private final UserService userService;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//    private final LogoutHandler logoutHandler;
 
-    public WebSecurityConfig(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public WebSecurityConfig(UserService userService, JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
         this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
     }
 
     @Bean
-    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorization) -> authorization
-                        .requestMatchers("/v*/api/registration/**").permitAll()
-                        .anyRequest().authenticated()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers("/v1/api/registration/**").hasAnyRole(ADMIN.name(), USER.name())
+
+                                .requestMatchers(GET, ADMIN_USER_ENDPOINT).hasAnyAuthority(ADMIN.name(), USER.name())
+                                .requestMatchers(POST, ADMIN_USER_ENDPOINT).hasAnyAuthority(ADMIN.name(), USER.name())
+                                .requestMatchers(PUT, ADMIN_USER_ENDPOINT).hasAnyAuthority(ADMIN.name(), USER.name())
+                                .requestMatchers(DELETE, ADMIN_USER_ENDPOINT).hasAnyAuthority(ADMIN.name(), USER.name())
+
+                                .requestMatchers(GET, ADMIN_PARKING_SPACE_ENDPOINT).hasRole(ADMIN_READ.name())
+                                .requestMatchers(POST, ADMIN_PARKING_SPACE_ENDPOINT).hasRole(ADMIN_CREATE.name())
+                                .requestMatchers(PUT, ADMIN_PARKING_SPACE_ENDPOINT).hasRole(ADMIN_UPDATE.name())
+                                .requestMatchers(DELETE, ADMIN_PARKING_SPACE_ENDPOINT).hasRole(ADMIN_DELETE.name())
+
+                                .requestMatchers(GET, ADMIN_VEHICLE_ENDPOINT).hasRole(ADMIN_READ.name())
+                                .requestMatchers(POST, ADMIN_VEHICLE_ENDPOINT).hasRole(ADMIN_CREATE.name())
+                                .requestMatchers(PUT, ADMIN_VEHICLE_ENDPOINT).hasRole(ADMIN_UPDATE.name())
+                                .requestMatchers(DELETE, ADMIN_VEHICLE_ENDPOINT).hasRole(ADMIN_DELETE.name())
+
+                                .anyRequest()
+                                .authenticated()
                 )
-                .httpBasic(withDefaults());
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//                .logout(logout ->
+//                        logout.logoutUrl("/api/v1/auth/logout")
+//                                .addLogoutHandler(logoutHandler)
+//                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+//                )
         return http.build();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder);
-        provider.setUserDetailsService(userService);
-        return provider;
-    }
 }
